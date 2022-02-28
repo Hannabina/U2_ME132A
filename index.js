96aa786d1558adc1f2d0fe6a52a52d153b977c91
Hanna Bina Nilsen <bina.99@live.se>
2022-02-28 13:30:04 +0100

    Added a database of marvel characters

---
+++ index.js
@@ -0,0 +1,86 @@
+"use script";
+
+let database = [
+    {
+        id: 1,
+        name: "Spiderman",
+        identity: "Peter Parker",
+        gender: "Male",
+        location: "Queens, New York City",
+        species: "Human mutate"
+    },
+    {
+        id: 2,
+        name: "Thor",
+        identity: "Thor Odinson",
+        gender: "Male",
+        location: "Asgard",
+        species: "God of thunder"
+    },
+    {
+        id: 3,
+        name: "Scarlet witch",
+        identity: "Wanda Maximoff",
+        gender: "Female",
+        location: "Sokovia",
+        species: "Witch"
+    },
+    {
+        id: 4,
+        name: "Black Widow",
+        identity: "Natasha Alianovna Romanova",
+        gender: "Female",
+        location: "Stalingrad, Russia",
+        species: "Human"
+    },
+    {
+        id: 5,
+        name: "Wasp",
+        identity: "Hope Van Dyne",
+        gender: "Female",
+        location: "San Francisco, California",
+        species: "Human"
+    },
+    {
+        id: 6,
+        name: "Hulk",
+        identity: "Dr. Robert Bruce Banner",
+        gender: "Male",
+        location: "Dayton, Ohio",
+        species: "Human mutate"
+    },
+    {
+        id: 7,
+        name: "Iron Man",
+        identity: "Tony Stark",
+        gender: "Male",
+        location: "Manhattan, New York",
+        species: "Human"
+    },
+    {
+        id: 8,
+        name: "Gamora",
+        identity: "Gamora Zen Whoberi Ben Titan",
+        gender: "Female",
+        location: "Zen-Whoberi",
+        species: "Zehoberei"   
+    },
+    {
+        id: 9,
+        name: "Captain America",
+        identity: "Steve Rogers",
+        gender: "Male",
+        location: "Manhattan, New York City",
+        species: "Human"   
+    },
+    {
+        id: 10,
+        name: "Venom",
+        identity: "Venom (Eddie Brock)",
+        gender: "Undefinded",
+        location: "Klyntar (Gorr's planet)",
+        species: "Symbiote"
+    }
+
+]
+
